250d44e14de1fce27b68fcb7d2cfeca978eb808b
Yashi Srivastava <yashisri2005@gmail.com>
2025-11-07 15:33:32 +0530

    Time: 7 ms (20.35%) | Memory: 9.7 MB (74.00%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -3,7 +3,6 @@ public:
     bool isAnagram(string s, string t) {
         int s1=s.length();
         int s2=t.length();
-        int found=0;
         if(s1!=s2)
         return false;
         else{
