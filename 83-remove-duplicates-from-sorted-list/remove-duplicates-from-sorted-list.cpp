07004a11b161181691f103a1b061f5efb4976b3c
Yashi Srivastava <yashisri2005@gmail.com>
2025-11-07 16:14:49 +0530

    Time: 0 ms (100.00%) | Memory: 16.1 MB (67.17%) - LeetSync

--- 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.cpp
+++ 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.cpp
@@ -14,11 +14,12 @@ public:
         if (head == NULL) 
             return head; 
         ListNode* temp = head;
-        while (temp->next != NULL) 
+        while (temp->next != NULL) {
             if (temp->val == temp->next->val)     
                 temp->next = temp->next->next;
             else 
                 temp =temp->next;
+        }
         return head;
     }
 };
