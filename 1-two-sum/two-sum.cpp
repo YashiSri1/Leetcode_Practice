47f97b96d224d82f97fe8da94b1d3d8a5717bab0
Yashi Srivastava <yashisri2005@gmail.com>
2026-03-06 22:01:10 +0530

    Time: 228 ms (5.24%) | Memory: 14.1 MB (66.28%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int n=nums.size();
+       int n=nums.size();
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
                 if(i!=j) 
@@ -12,5 +12,6 @@ public:
             }
         }
         return {};
+    
     }
 };
