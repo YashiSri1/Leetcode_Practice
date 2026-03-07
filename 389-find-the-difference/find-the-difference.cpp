f8d04428352ace07279bef17314bbd93ef4b4fc3
Yashi Srivastava <yashisri2005@gmail.com>
2026-03-07 23:25:13 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (76.12%) - LeetSync

--- 389-find-the-difference/find-the-difference.cpp
+++ 389-find-the-difference/find-the-difference.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     char findTheDifference(string s, string t) {
         char res='\0';
-        for(auto &c:s) 
-        res^=c;
-        for(auto &c:t)
-        res^=c;
+        for(int i=0;i<s.length();i++) 
+        res^=s[i];
+        for(int i=0;i<t.length();i++) 
+        res^=t[i];
         return res;
     }
 };
