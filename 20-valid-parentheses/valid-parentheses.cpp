e74009bf1bb7ef9718bca7b857b49a7b899bc4b6
Yashi Srivastava <yashisri2005@gmail.com>
2026-07-07 17:06:59 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (38.00%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool isValid(string s) {
-         stack<char> st;
+        stack<char> st;
         for (char ch : s) {
             if (ch == '(' || ch == '[' || ch == '{') {
                 st.push(ch);
