1f01300ce2795270f106275e49a77b1af5ea1959
Yashi Srivastava <yashisri2005@gmail.com>
2026-08-17 22:28:13 +0530

    Time: 0 ms (100.00%) | Memory: 15 MB (16.81%) - LeetSync

--- 1677-matrix-diagonal-sum/matrix-diagonal-sum.cpp
+++ 1677-matrix-diagonal-sum/matrix-diagonal-sum.cpp
@@ -5,10 +5,10 @@ public:
         int sum = 0;
 
         for(int i=0;i<n;i++){
-            sum += mat[i][i] + mat[i][n-i-1];
+            sum += mat[i][i] ;
+        if(i != n-1-i) 
+        sum += mat[i][n-1-i];
         }
-
-        if(n%2 == 1) sum -= mat[n/2][n/2];
 
         return sum;
     }
